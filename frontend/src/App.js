8a7c0389da1d422d61b7446228a8f3a57e7f83bc
Osvaldino Neto <alfnet.design@gmail.com>
2020-04-01 08:15:49 -0400

    Front-end do app web

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,16 +1,13 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+import './global.css';
+
+import Routes from './routes';
+
+function App() {  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello OmniStack.
-        </p>
-      </header>
+    <div>
+      <Routes />
     </div>
   );
 }
